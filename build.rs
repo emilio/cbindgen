c4248123b8c10efc7bd4b66ea561158a10c906fc
Emilio Cobos Álvarez <emilio@crisal.io>
2019-03-30 16:47:21 +0100

    WIP: Port tests to use cargo test.

---
+++ build.rs
@@ -0,0 +1,47 @@
+fn generate_tests() {
+    use std::env;
+    use std::path::{Path, PathBuf};
+    use std::fs::{self, File};
+    use std::ffi::OsStr;
+    use std::io::Write;
+
+    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+    let mut dst = File::create(Path::new(&out_dir).join("tests.rs")).unwrap();
+
+    let manifest_dir =
+        PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
+    let tests_dir = manifest_dir.join("tests").join("rust");
+    let tests = fs::read_dir(&tests_dir).unwrap();
+
+    let entries = tests.map(|t| t.expect("Couldn't read test file"));
+
+    println!("cargo:rerun-if-changed={}", tests_dir.display());
+
+    for entry in entries {
+        // TODO(emilio): handle directories.
+        match entry.path().extension().and_then(OsStr::to_str) {
+            Some("rs") => {},
+            _ => continue,
+        };
+
+        let identifier = entry
+            .file_name()
+            .to_str()
+            .unwrap()
+            .replace(|c| !char::is_alphanumeric(c), "_")
+            .replace("__", "_")
+            .to_lowercase();
+        writeln!(
+            dst,
+            "test_file!({}, {:?});",
+            identifier,
+            entry.path(),
+        ).unwrap();
+    }
+
+    dst.flush().unwrap();
+}
+
+fn main() {
+    generate_tests();
+}
