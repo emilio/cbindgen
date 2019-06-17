c6833f266884166b5912c08fc4a0c8492af01929
Emilio Cobos Álvarez <emilio@crisal.io>
2019-06-16 19:49:45 -0700

    Add an option to parse items outside of the binding crate.
    This can be useful.

--- src/bindgen/library.rs
+++ src/bindgen/library.rs
@@ -77,12 +77,11 @@ impl Library {
         self.constants.for_all_items(|constant| {
             constant.add_dependencies(&self, &mut dependencies);
         });
+
         for name in &self.config.export.include {
             let path = Path::new(name.clone());
             if let Some(items) = self.get_items(&path) {
-                if !dependencies.items.contains(&path) {
-                    dependencies.items.insert(path);
-
+                if dependencies.items.insert(path) {
                     for item in &items {
                         item.deref().add_dependencies(&self, &mut dependencies);
                     }
